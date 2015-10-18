168ae7b8b6a5a11cbd8379025353d8fc49c6214b
Abdelkader Boudih <terminale@gmail.com>
2015-10-18 11:51:47 +0100

    new travis config

--- travis/before_install.sh
+++
@@ -1,5 +0,0 @@
-echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
-
-sudo apt-get update
-sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.4-postgis  libgeos++-dev
-
